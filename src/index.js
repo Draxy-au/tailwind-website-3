cc0fbbca25779595af0a59cd89b1de97065c9345
William Hamilton <draxy80@gmail.com>
2022-08-12 05:04:17 +1000

    Added Form Validation. Added Statistics Section. Added boxes to Stats section.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import "./input.css";
+
+const uiForm = document.querySelector("#form");
+const uiInput = document.querySelector("#form-input");
+const uiError = document.querySelector("#form-error");
+
+uiForm.addEventListener("submit", (e) => formSubmit(e));
+
+// Validate a URL
+function validURL(str) {
+  var pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+      "((\\d{1,3}\\.){3}\\d{1,3}))" +
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+      "(\\?[;&a-z\\d%_.~+=-]*)?" +
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  );
+  return !!pattern.test(str);
+}
+
+function formSubmit(e) {
+  e.preventDefault();
+
+  console.log(uiInput.value);
+
+  if (uiInput.value === "") {
+    uiError.innerHTML = "Please enter something";
+    uiInput.classList.add("border-red");
+  } else if (!validURL(uiInput.value)) {
+    uiError.innerHTML = "Please enter a valid URL";
+    uiInput.classList.add("border-red");
+  } else {
+    uiError.innerHTML = "";
+    uiInput.classList.remove("border-red");
+    alert("Success");
+  }
+}
